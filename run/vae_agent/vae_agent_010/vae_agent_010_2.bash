84c833885b2276a2a06d7433bec64ec48d43a0de
zhufengdaaa <zhufengdaaa@gmail.com>
2019-09-27 10:22:06 +0000

    optimize bash folder architecture

---
+++ run/vae_agent/vae_agent_010/vae_agent_010_2.bash
@@ -0,0 +1,14 @@
+# simplify the structure of policy decoder, no preload, no fix
+# add action emb for decoder
+# add dropout
+name=$(echo $0 | cut -d. -f1 | cut -d/ -f2)
+flag="--attn soft --train vae_agent
+      --featdropout 0.3
+      --angleFeatSize 128
+      --feedback sample
+      --mlWeight 0.2
+      --subout max --dropout 0.5 --optim rms --lr 1e-4 --iters 80000 
+      --maxAction 35
+      "
+mkdir -p snap/$name
+CUDA_VISIBLE_DEVICES=$1 python3.6 r2r_src/train.py $flag --name $name 
